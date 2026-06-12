61ba8d144b0eb0c224ae4bc7029fa10981c88ac0
DenisKoriavets <d.korjavets@ukr.net>
2026-06-12 14:04:22 +0300

    add Checkstyle configuration and CI/CD pipeline for code analysis and testing

--- homework-5/build.gradle.kts
+++ homework-5/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("checkstyle")
 }
 
 group = "ua.edu.ukma"
@@ -13,6 +14,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
 }
 
 tasks.test {
